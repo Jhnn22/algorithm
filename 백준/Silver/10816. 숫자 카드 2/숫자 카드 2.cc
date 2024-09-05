7cd6efee501d4db6076313140e88ffa59408fbe0
Jhnn22 <jhnn22@naver.com>
2024-09-05 18:21:39 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 804 ms, Memory: 25176 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#define FAST_IO cin.tie(NULL)->ios::sync_with_stdio(false);
+using namespace std;
+
+int main(){
+    int N, n1;
+    cin >> N;
+    unordered_map<int, int> freq;
+    for(int i = 0; i < N; i++){
+        cin >> n1;
+        freq[n1]++;
+    }
+    
+    int M, n2;
+    cin >> M;
+    vector<int> answer(M, 0);
+    for(int i = 0; i < M; i++){
+        cin >> n2;
+        auto it = freq.find(n2);
+        if(it != freq.end()){
+            answer[i] = it->second;
+        }
+    }
+    for(const int& n : answer){
+        cout << n << ' ';
+    }
+    
+    return 0;
+}
