488ebb1885c923cefa46f62da2552fccd000a6dc
Jhnn22 <jhnn22@naver.com>
2024-09-10 23:54:30 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    int n = citations.size();
+    sort(citations.begin(), citations.end(), [](int a, int b){
+       return a > b; 
+    });
+    
+    for(int i = 0; i < n; i++){
+        if(citations[i] >= i + 1){
+            answer = i + 1;
+        }
+    } 
+    
+    return answer;
+}
